ed5cd91e488cf8902dc0517739bab5b212d8fb87
Elen-tesfai <Elen_tes@hotmail.com>
2024-08-30 17:15:19 -0500

    Update utils_elen.py

--- utils_elen.py
+++ utils_elen.py
@@ -1,5 +1,15 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
 ''' ITERATION 1
-A
+
 Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
 
 This module provides a simple, reusable foundation for my analytics projects. 
