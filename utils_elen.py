b8916277ad8ea0e36baefb993704c65d77b27867
Elen-tesfai <Elen_tes@hotmail.com>
2024-09-05 18:45:45 -0500

    Iteration 4

--- utils_elen.py
+++ utils_elen.py
@@ -149,6 +149,98 @@ def get_byline() -> str:
 # Define a main() function for this module.
 #########################################
 
+''' ITERATION 4
+ 
+ Module:'Elen Analytics: Delivering Professional Insights'
+
+Process:
+In this fourth iteration, I introduce some basic stats using Python.
+    - min () is a built in function to find the smallest value passed in 
+    - max is a built in function to find the largegst value passed in
+    - The statistics module offers methods to calculate mean and standard deviation
+    '''
+    
+###########################
+    # Import Modules at the Top
+    ###########################
+    
+    # In Python, we can import modules to add extra tools and functions.
+    # Below, we're importing:
+    # - 'statistics: This gives us tools to calculate things like averages.
+    # Use CTRL F and type statistics to see where it is used in the code.
+    # Did you find statistics.mean()?
+    # Did you find statistics.stdev() ?
+    
+import statistics
+    
+    ##########################
+    # Declare global variables
+    ###########################
+    
+    # Boolean variable to indicate if the company has international Clients
+has_international_clients: bool = True
+    
+    # Integer variable for the number of years in Operation
+years_in_operation: int = 10
+    
+    # Float variable for teh average client satisfaction Score
+average_client_satisfaction: float = 4.7
+    
+    # List of strings representing the skills offered by the company
+skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+    
+    #List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+  #####################################
+    # Calculate Basic Statistics
+    # Do this BEFORE we declare the byline
+    # So the values have been calculated 
+     # and are ready for use in the byline.
+    #####################################  
+   
+    # Calculate basic stats using built-in functions min (), max() and statistics module functions mean () and stdev().
+min_score: float = min(client_satisfaction_scores)
+max_score: float = max(client_satisfaction_scores)
+stdev_score: float = statistics.stdev(client_satisfaction_scores)
+    
+    #####################################
+    # Declare a global variable named byline
+    # Make it a multiline f-string to show our information
+    #####################################
+    
+byline: str = f"""
+
+-----------------------------------------------------------
+Elen Analytics: Delivering Professional Insights
+----------------------------------------------------------
+
+Has International Clients: {has_international_clients}
+Years in Operation:      {years_in_operation}
+Skills Offered:           {skills_offered }
+Client Satisfaction Scores: {client_satisfaction_scores}
+"""
+#####################################
+# Define the get_byline() function
+#####################################  
+
+def get_byline() -> str:
+    '''Return a buline for my analytics projects.'''
+    return byline
+#####################################
+# Define a main () function for this module
+#####################################  
+def main () -> None:
+    '''Print results of get_byline() when main() is called.'''
+print(get_byline())
+
+#########################################
+# Conditional Execution
+#########################################
+
+if __name__ == '__main__':
+    main()
+
+
 def main() -> None:
     '''Print results of get_byline() when main() is called.'''
     print(get_byline())
